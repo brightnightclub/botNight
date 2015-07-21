f7471a2c75552932f53a4be2281294dbb676a8bf
Ariel Spear, Emily Bookstein, Sam Julien, Spyder <simpleredfox@gmail.com>
2015-07-20 20:16:55 -0700

    conversation has begun

--- groups_bots/imposterBot/botness.rb
+++ groups_bots/imposterBot/botness.rb
@@ -1,1 +1,18 @@
-puts "Hello Friend, have a magical day."
+QUITTERS = ["exit", "quit", "x", "q", "bye"]
+
+puts "Hello Friend, how can I help?"
+
+running = true
+
+def process(input)
+  if QUITTERS.include? input.downcase
+    puts "Goodbye"
+    exit
+  end
+  puts "Hhhhhhmmmmmm, interesting thought..."
+end
+
+while running do
+  input = gets.chomp
+  process(input)
+end
